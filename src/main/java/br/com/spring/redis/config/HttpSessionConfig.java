66962cbf4b29684a6a5dc0f3fe3bc533327c70a2
eskeff <eskeff@NB-002651.cwinet.local>
2016-07-04 16:02:24 -0300

    Desabilitar o Redis

--- src/main/java/br/com/spring/redis/config/HttpSessionConfig.java
+++ src/main/java/br/com/spring/redis/config/HttpSessionConfig.java
@@ -1,14 +1,12 @@
 package br.com.spring.redis.config;
 
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-@EnableRedisHttpSession
+//@EnableRedisHttpSession
 public class HttpSessionConfig {
 	
 //	@Bean
 //    public JedisConnectionFactory connectionFactory() {
 //			  JedisConnectionFactory cf = new JedisConnectionFactory();
-//			  cf.setHostName("10.0.75.2");
+//			  cf.setHostName("host");
 //			  cf.setPort(6379);
 //			  return cf;
 //    }
