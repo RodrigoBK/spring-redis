bd03773ac480570389e5d31269dcb64994d33086
eskeff <eskeff@NB-002651.cwinet.local>
2016-07-12 09:33:35 -0300

    Fix

--- src/main/java/br/com/spring/redis/config/HttpSessionConfig.java
+++ src/main/java/br/com/spring/redis/config/HttpSessionConfig.java
@@ -6,7 +6,7 @@ public class HttpSessionConfig {
 //	@Bean
 //    public JedisConnectionFactory connectionFactory() {
 //			  JedisConnectionFactory cf = new JedisConnectionFactory();
-//			  cf.setHostName("host");
+//			  cf.setHostName("localhost");
 //			  cf.setPort(6379);
 //			  return cf;
 //    }
